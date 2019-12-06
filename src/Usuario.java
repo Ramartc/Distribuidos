71d486cc515922953a6ab80f1db8e80fb48d7622
algonzsen <1qpWM9z2>
2019-12-06 19:02:06 +0100

    clase usuario creada

---
+++ src/Usuario.java
@@ -0,0 +1,29 @@
+
+public class Usuario {
+	private int id;
+	private String user;
+	private String password;
+	public Usuario(String u,String p,int id) {
+		//Buscar en el fichero de usuarios si este usuario existe
+		this.user = u;
+		this.password = p;
+		this.id = id;//numero que este pa este usuario en el fichero 
+	}
+	public String getUser() {
+		return user;
+	}
+	public void setUser(String user) {
+		//cada vez que se haga un set actualizar el fichero
+		this.user = user;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		//cada vez que se haga un set actualizar el fichero
+		this.password = password;
+	}
+	public int getId() {
+		return id;
+	}
+}
